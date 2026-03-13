ab463e37fd87e3bc7a285b58505db3cf149aa249
vedangiitb <vedangbaleiitb@gmail.com>
2026-03-13 22:05:05 +0530

    updated redis secrets

--- config/redis.ts
+++ config/redis.ts
@@ -2,6 +2,6 @@
 import { Redis } from "@upstash/redis";
 
 export const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL!,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+  url: process.env.UPSTASH_REDIS_REST_URL_GEN_EVENTS!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN_GEN_EVENTS!,
 });
